35f392cffdd84574c662b7dfb5fe36b5fb6f5a7c
Ashish Kumar <ashish29300@gmail.com>
2026-02-04 19:59:47 +0530

    Added Database for Memory

--- chatbot.py
+++ chatbot.py
@@ -1,8 +1,9 @@
 from langgraph.graph import StateGraph, START, END
 from langchain_openai import ChatOpenAI
-from langgraph.checkpoint.memory import InMemorySaver
+from langgraph.checkpoint.sqlite import SqliteSaver
 from dotenv import load_dotenv
 from state import ChatState
+import sqlite3
 
 # Load environment variables from .env file
 load_dotenv()
@@ -16,7 +17,9 @@ def chat_node(state: ChatState):
     return {'messages': [response]}
 
 # In-memory checkpointer
-checkpointer = InMemorySaver()
+conn = sqlite3.connect(database="chatbot.db", check_same_thread=False)
+checkpointer = SqliteSaver(conn = conn)
+
 
 # Build a simple graph
 graph = StateGraph(ChatState)
@@ -27,3 +30,12 @@ graph.add_edge('chat_node', END)
 
 # Compile the graph
 chatbot = graph.compile(checkpointer=checkpointer)
+
+
+# ----------- Utility Functions -----------
+def retrieve_all_threads():
+    all_threads = set()
+    for checkpoint in checkpointer.list(None):
+        all_threads.add(checkpoint.config['configurable']['thread_id'])
+    
+    return list(all_threads)
